42dc8b6d622be648a56a93f33e6410912b82e617
Fareed Matovu <fareedmkim@gmail.com>
2023-11-17 17:50:31 +0300

    progress

---
+++ Final submission.R
@@ -0,0 +1,478 @@
+
+#Import packages
+## naniar ----
+# Documentation:
+#   https://cran.r-project.org/package=naniar or
+#   https://www.rdocumentation.org/packages/naniar/versions/1.0.0
+if (!is.element("naniar", installed.packages()[, 1])) {
+  install.packages("naniar", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("naniar")
+
+if (require("caret")) {
+  require("caret")
+} else {
+  install.packages("caret", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+
+if (!is.element("dplyr", installed.packages()[, 1])) {
+  install.packages("dplyr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("dplyr")
+
+## ggplot2 - For data visualizations using the Grammar for Graphics package ----
+if (!is.element("ggplot2", installed.packages()[, 1])) {
+  install.packages("ggplot2", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("ggplot2")
+
+## ggrepel - Additional options for the Grammar for Graphics package ----
+if (!is.element("ggrepel", installed.packages()[, 1])) {
+  install.packages("ggrepel", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("ggrepel")
+
+## ggraph - Additional options for the Grammar for Graphics package ----
+if (!is.element("ggraph", installed.packages()[, 1])) {
+  install.packages("ggraph", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("ggraph")
+
+## tidytext - For text mining ----
+if (!is.element("tidytext", installed.packages()[, 1])) {
+  install.packages("tidytext", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("tidytext")
+
+## tidyr - To tidy messy data ----
+if (!is.element("tidyr", installed.packages()[, 1])) {
+  install.packages("tidyr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("tidyr")
+
+## widyr - To widen, process, and re-tidy a dataset ----
+if (!is.element("widyr", installed.packages()[, 1])) {
+  install.packages("widyr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("widyr")
+
+## gridExtra - to arrange multiple grid-based plots on a page ----
+if (!is.element("gridExtra", installed.packages()[, 1])) {
+  install.packages("gridExtra", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("gridExtra")
+
+## knitr - for dynamic report generation ----
+if (!is.element("knitr", installed.packages()[, 1])) {
+  install.packages("knitr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("knitr")
+
+## kableExtra - for nicely formatted output tables ----
+if (!is.element("kableExtra", installed.packages()[, 1])) {
+  install.packages("kableExtra", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("kableExtra")
+
+## formattable -  To create a formattable object ----
+# A formattable object is an object to which a formatting function and related
+# attributes are attached.
+if (!is.element("formattable", installed.packages()[, 1])) {
+  install.packages("formattable", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("formattable")
+
+## circlize - To create a cord diagram or visualization ----
+# by Gu et al. (2014)
+if (!is.element("circlize", installed.packages()[, 1])) {
+  install.packages("circlize", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("circlize")
+
+## memery - For creating data analysis related memes ----
+# The memery package generates internet memes that optionally include a
+# superimposed inset plot and other atypical features, combining the visual
+# impact of an attention-grabbing meme with graphic results of data analysis.
+if (!is.element("memery", installed.packages()[, 1])) {
+  install.packages("memery", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("memery")
+
+## magick - For image processing in R ----
+if (!is.element("magick", installed.packages()[, 1])) {
+  install.packages("magick", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("magick")
+
+## yarrr - To create a pirate plot ----
+if (!is.element("yarrr", installed.packages()[, 1])) {
+  install.packages("yarrr", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("yarrr")
+
+## radarchart - To create interactive radar charts using ChartJS ----
+if (!is.element("radarchart", installed.packages()[, 1])) {
+  install.packages("radarchart", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("radarchart")
+
+## igraph - To create ngram network diagrams ----
+if (!is.element("igraph", installed.packages()[, 1])) {
+  install.packages("igraph", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("igraph")
+
+## wordcloud2 - For creating wordcloud by using 'wordcloud2.JS ----
+if (!is.element("wordcloud2", installed.packages()[, 1])) {
+  install.packages("wordcloud2", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+require("wordcloud2")
+
+if (!is.element("mlbench", installed.packages()[, 1])) {
+  install.packages("mlbench", dependencies = TRUE)
+}
+require("mlbench")
+
+if (!is.element("e1071", installed.packages()[, 1])) {
+  install.packages("e1071", dependencies = TRUE)
+}
+require("e1071")
+
+if (require("randomForest")) {
+  require("randomForest")
+} else {
+  install.packages("randomForest", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+if (require("RRF")) {
+  require("RRF")
+} else {
+  install.packages("RRF", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## plumber ----
+if (require("plumber")) {
+  require("plumber")
+} else {
+  install.packages("plumber", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+#Import data set 
+library(readr)
+Food_demand <- read_csv("data/Food demand.csv", col_types = cols(
+  homepage_featured = col_factor(levels = c("0","1")),
+  emailer_for_promotion = col_factor(levels = c("0","1")),
+  num_orders = col_double()
+))
+View(Food_demand)
+
+
+
+#Measure of frequency
+food_demand_homepage_featured_freq <- Food_demand$homepage_featured
+cbind(frequency = table(food_demand_freq),
+      percentage = prop.table(table(food_demand_freq)) * 100)
+
+
+food_demand_email_promotion_freq <- Food_demand$emailer_for_promotion
+cbind(frequency = table(food_demand_freq),
+      percentage = prop.table(table(food_demand_freq)) * 100)
+
+
+#Measures of central tendency 
+#Mode
+food_demand_number_orders_mode <- names(table(Food_demand$num_orders))[
+  which(table(Food_demand$num_orders) == max(table(Food_demand$num_orders)))
+]
+print(food_demand_number_orders_mode)
+
+
+#Measure of distribution 
+#standard deviation
+sapply(Food_demand[, c(5,6)], sd)
+#variance
+sapply(Food_demand[, c(5,6)], var)
+#Kurtosis
+sapply(Food_demand[, c(5,6)], kurtosis, type =2)
+#skewness
+sapply(Food_demand[, c(5,6)], skewness, type = 2)
+
+#Neasure of relationship
+
+#covariance
+food_demand_cov <- cov(Food_demand[, c(5,6)])
+View(food_demand_cor)
+
+
+#correlation
+food_demand_cor <- cor(Food_demand[, c(5,6)])
+View(food_demand_cor)
+
+
+
+#Check for missing data 
+any_na(Food_demand)
+#false
+
+#check how many
+n_miss(Food_demand)
+#0 missing data 
+
+
+
+# Create a histogram
+
+hist(Food_demand$num_orders, main = "Histogram of Data", xlab = "Values", col = "skyblue", border = "black")
+boxplot(Food_demand$num_orders, main = "Boxplot of Data", xlab = "Values", col = "lightgreen", border = "black")
+
+
+data <- data.frame(x = Food_demand$base_price, y = Food_demand$checkout_price)
+
+# Create a ggplot scatter plot
+ggplot(data, aes(x = x, y = y)) +
+  geom_point(color = "blue") +
+  labs(title = "Scatter Plot of Base price against checkout price", x = "Base price", y = "Checkout price")
+
+# Sample data with two variables
+data <- data.frame(x = Food_demand$checkout_price, y = Food_demand$num_orders)
+
+# Create a boxplot for each variable
+boxplot(data, main = "Boxplot of checkout price and number of orders", col = c("lightblue", "lightgreen"), border = "black")
+
+
+#Anova
+
+
+
+
+
+
+#Data transformation
+
+#standardize 
+
+#Before
+sapply(Food_demand[, c(5,6)], sd)
+
+model_of_the_transform <- preProcess(Food_demand,
+                                     method = c("scale", "center"))
+print(model_of_the_transform)
+food_demand_standardize_transform <- predict(model_of_the_transform, # nolint
+                                                Food_demand)
+
+# AFTER
+summary(food_demand_standardize_transform)
+sapply(food_demand_standardize_transform[, c(5,6)], sd)
+
+
+
+#Data splitting 
+# Define an 80:20 train:test data split of the dataset.
+train_index <- createDataPartition(Food_demand$num_orders,
+                                   p = 0.8,
+                                   list = FALSE)
+food_demand_train <- Food_demand[train_index, ]
+food_demand_test <- Food_demand[-train_index, ]
+
+
+
+
+#Bootstrapping
+train_control <- trainControl(method = "boot", number = 500)
+
+food_demand_dataset_model_lm <- # nolint
+  caret::train(num_orders ~ .,
+               data = food_demand_train,
+               trControl = train_control,
+               na.action = na.omit, method = "lm", metric = "RMSE")
+
+## 3. Test the trained linear regression model using the testing dataset ----
+predictions_lm <- predict(food_demand_dataset_model_lm,
+                          food_demand_test[, 1:9])
+
+## 4. View the RMSE and the predicted values for the 12 observations ----
+print(food_demand_dataset_model_lm)
+print(predictions_lm)
+
+
+#Cross Validation
+train_control <- trainControl(method = "cv", number = 10)
+
+food_dateset_model_lm <-
+  caret::train(num_orders ~ .,
+               data = food_demand_train,
+               trControl = train_control, na.action = na.omit,
+               method = "lm", metric = "RMSE")
+
+### 2.b. Test the trained linear model using the testing dataset ----
+predictions_lm <- predict(food_dateset_model_lm, food_demand_test[, 1:9])
+
+### 2.c. View the RMSE and the predicted values ====
+print(food_dateset_model_lm)
+print(predictions_lm)
+
+
+## LINEAR regression without Carret 
+#### Train the model ----
+food_demand_model_lm <- lm(num_orders ~ ., food_demand_train)
+
+
+#### Make predictions ----
+predictions <- predict(food_demand_model_lm, food_demand_test[, 1:9])
+
+
+rmse <- sqrt(mean((food_demand_test$num_orders - predictions)^2))
+print(paste("RMSE =", sprintf(rmse, fmt = "%#.4f")))
+
+ssr <- sum((food_demand_test$num_orders - predictions)^2)
+print(paste("SSR =", sprintf(ssr, fmt = "%#.4f")))
+
+##### SST ----
+# SST is the total sum of squares (the sum of squared differences
+# between observed values and their mean)
+sst <- sum((food_demand_test$num_orders - mean(food_demand_test$num_orders))^2)
+print(paste("SST =", sprintf(sst, fmt = "%#.4f")))
+
+r_squared <- 1 - (ssr / sst)
+print(paste("R Squared =", sprintf(r_squared, fmt = "%#.4f")))
+
+
+absolute_errors <- abs(predictions - food_demand_test$num_orders)
+mae <- mean(absolute_errors)
+print(paste("MAE =", sprintf(mae, fmt = "%#.4f")))
+
+
+
+#Linear Regression with carret
+train_index <- createDataPartition(Food_demand$num_orders,
+                                   p = 0.8,
+                                   list = FALSE)
+Food_demand_train <- Food_demand[train_index, ]
+Food_demand_test <- Food_demand[-train_index, ]
+
+#### Train the model ----
+set.seed(7)
+train_control <- trainControl(method = "cv", number = 5)
+food_demand_caret_model_lm <- train(num_orders ~ ., data = Food_demand_train,
+                                       method = "lm", metric = "RMSE",
+                                       preProcess = c("center", "scale"),
+                                       trControl = train_control)
+
+#### Display the model's details ----
+print(food_demand_caret_model_lm)
+
+#### Make predictions ----
+predictions <- predict(food_demand_caret_model_lm,
+                       Food_demand_test[, 1:9])
+
+#### Display the model's evaluation metrics ----
+##### RMSE ----
+rmse <- sqrt(mean((Food_demand_test$num_orders - predictions)^2))
+print(paste("RMSE =", sprintf(rmse, fmt = "%#.4f")))
+
+##### SSR ----
+# SSR is the sum of squared residuals (the sum of squared differences
+# between observed and predicted values)
+ssr <- sum((Food_demand_test$num_orders - predictions)^2)
+print(paste("SSR =", sprintf(ssr, fmt = "%#.4f")))
+
+##### SST ----
+# SST is the total sum of squares (the sum of squared differences
+# between observed values and their mean)
+sst <- sum((Food_demand_test$num_orders - mean(Food_demand_test$num_orders))^2)
+print(paste("SST =", sprintf(sst, fmt = "%#.4f")))
+
+##### R Squared ----
+# We then use SSR and SST to compute the value of R squared.
+# The closer the R squared value is to 1, the better the model.
+r_squared <- 1 - (ssr / sst)
+print(paste("R Squared =", sprintf(r_squared, fmt = "%#.4f")))
+
+##### MAE ----
+# MAE is expressed in the same units as the target variable, making it easy to
+# interpret. For example, if you are predicting the amount paid in rent,
+# and the MAE is KES. 10,000, it means, on average, your model's predictions
+# are off by about KES. 10,000.
+absolute_errors <- abs(predictions - Food_demand_test$num_orders)
+mae <- mean(absolute_errors)
+print(paste("MAE =", sprintf(mae, fmt = "%#.4f")))
+
+
+
+
+
+print(food_demand_caret_model_lm$finalModel)
+
+
+set.seed(9)
+predictions <- predict(food_demand_caret_model_lm, newdata = food_demand_test)
+
+
+# STEP 5. Save and Load your Model ----
+# Saving a model into a file allows you to load it later and use it to make
+# predictions. Saved models can be loaded by calling the `readRDS()` function
+
+saveRDS(food_demand_caret_model_lm, "./saved_food_demand_caret_model_lda.rds")
+# The saved model can then be loaded later as follows:
+loaded_demand_model_caret_lr <- readRDS("./saved_food_demand_caret_model_lda.rds")
+print(loaded_demand_model_caret_lr)
+
+predictions_with_loaded_model <-
+  predict(loaded_demand_model_caret_lr, newdata = food_demand_test)
+
+ii
+# STEP 6. Creating Functions in R ----
+
+# Plumber requires functions, an example of the syntax for creating a function
+# in R is:
+
+
+predict_num_orders <-
+  function(arg_week,arg_center_id, arg_meal_id, arg_checkout_price, arg_base_price,  arg_email_promotion, arg_homepage_feature) {
+    # Create a data frame using the arguments
+    to_be_predicted <-
+      data.frame(arg_week = week, arg_center_id = ceter_id, arg_meal_id, checkout_price = arg_checkout_price, base_price = arg_base_price,  emailer_for_promotion = arg_email_promotion, homepage_featured = arg_homepage_feature, )
+    
+    # Make a prediction based on the data frame
+    predict(loaded_demand_model_caret_lr, to_be_predicted)
+  }
+
+
+# We can now call the function predict_diabetes() instead of calling the
+# predict() function directly
+
+predict_num_orders(100, 150, 1971,200, 250, 0, 1)
+
+
+
+
+
+
+
+
+
+
+
